92620c70cb66aa3f704b36f1ce3b4eb34c26faa0
Christopher Rescorla <cgrescorla@gmail.com>
2016-12-15 10:13:37 -0700

    many additions, also split component from app.js

--- js/app.js
+++ js/app.js
@@ -2,53 +2,8 @@
  * Created by Godai Yuusaku on 12/14/2016.
  */
 (function () {
-    var myApp = angular.module("myApp", ["ui.router"]);
+    angular.module("myApp", ["ui.router"])
+        .config(function ($stateProvider, $urlRouterProvider) {
 
-    myApp.config(function ($stateProvider, $urlRouterProvider) {
-
-    });
-
-    myApp.component("basePage", {
-        // template: "<h2>{{vm.testing}}</h2>",
-        templateUrl: "html/basePage.html",
-        controller: basePageController,
-        controllerAs: "vm"
-    });
-
-    function basePageController() {
-        this.testing = "Boo! From Controller!";
-        this.listArray = [];
-        this.currentList;
-        this.newList;
-        this.newItem;
-
-        this.addList = function () {
-            if (this.newList) {
-                this.listArray.push({name: this.newList, items: []});
-                this.newList = "";
-                this.currentList = this.listArray[this.listArray.length - 1];
-
-            }
-        };
-
-        this.getList = function (listName) {
-            for (var i = 0; i < this.listArray.length; i++) {
-                if (listName === this.listArray[i].name) {
-                    this.currentList = this.listArray[i];
-                }
-            }
-        };
-
-        this.clear = function ()
-        {
-            this.currentList.items = [];
-        };
-
-        this.addItem = function () {
-            if (this.newItem && (this.currentList.items.indexOf(this.newItem) === -1)) {
-                this.currentList.items.push(this.newItem);
-                this.newItem = "";
-            }
-        }
-    }
+        });
 })();
---
+++ js/basePage.component.js
@@ -0,0 +1,70 @@
+/**
+ * Created by GodaiYuusaku on 12/15/16.
+ */
+(function () {
+    angular
+        .module("myApp")
+        .component("basePage", {
+            templateUrl: "html/basePage.html",
+            controller: basePageController,
+            controllerAs: "vm"
+        });
+
+    function basePageController() {
+        this.testing = "Boo! From Controller!";
+        this.listArray = [];
+        this.currentList;
+        this.newList;
+        this.newItem;
+
+        this.addList = function () {
+            if (this.newList) {
+                var inList = false;
+                for (var i = 0; i < this.listArray.length; i++) {
+                    if (this.newList === this.listArray[i].name) {
+                        inList = true;
+                        break;
+                    }
+                }
+                if (!inList) {
+                    this.listArray.push({name: this.newList, items: []});
+                    this.currentList = this.listArray[this.listArray.length - 1];
+                }
+                this.newList = "";
+            }
+        };
+
+        this.getList = function (listName) {
+            for (var i = 0; i < this.listArray.length; i++) {
+                if (listName === this.listArray[i].name) {
+                    this.currentList = this.listArray[i];
+                }
+            }
+        };
+
+        this.clear = function () {
+            this.currentList.items = [];
+            this.selected = null;
+        };
+
+        this.addItem = function () {
+            if (this.newItem && (this.currentList.items.indexOf(this.newItem) === -1)) {
+                this.currentList.items.push(this.newItem);
+                this.newItem = "";
+            }
+        };
+
+        this.removeItem = function (item) {
+            this.currentList.items.splice(this.currentList.items.indexOf(item), 1);
+        };
+
+        this.clearCompleted = function () {
+            for (var key in this.selected) {
+                if (this.currentList.items.indexOf(key) != -1 && this.selected[key]) {
+                    this.currentList.items.splice(this.currentList.items.indexOf(key), 1);
+                    delete this.selected[key];
+                }
+            }
+        };
+    }
+})();
