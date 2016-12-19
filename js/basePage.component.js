cb72d1b77ad5426d279a39932c57cade9761b1c7
Christopher Rescorla <cgrescorla@gmail.com>
2016-12-18 19:15:21 -0700

    now with a service and more formatting

--- js/basePage.component.js
+++ js/basePage.component.js
@@ -6,70 +6,118 @@
         .module("myApp")
         .component("basePage", {
             templateUrl: "html/basePage.html",
+            // service: BasePageService,
             controller: basePageController,
             controllerAs: "vm"
+        })
+        .service("BasePageService", function () {
+            this.listArray = [];
+            this.currentList;
+            this.newItem;
+            this.selected = null;
+
+            this.addList = function (newList) {
+                if (newList) {
+                    var inList = false;
+                    for (var i = 0; i < this.listArray.length; i++) {
+                        if (newList === this.listArray[i].name) {
+                            inList = true;
+                            break;
+                        }
+                    }
+                    if (!inList) {
+                        this.listArray.push({name: newList, items: []});
+                        this.currentList = this.listArray[this.listArray.length - 1];
+                    }
+                }
+            };
+
+            this.getList = function (listName) {
+                for (var i = 0; i < this.listArray.length; i++) {
+                    if (listName === this.listArray[i].name) {
+                        this.currentList = this.listArray[i];
+                        break;
+                    }
+                }
+            };
+
+            this.clear = function () {
+                this.currentList.items = [];
+                // this.selected = null;
+            };
+
+            this.addItem = function (newItem) {
+                if (newItem && (this.currentList.items.indexOf(newItem) === -1)) {
+                    this.currentList.items.push(newItem);
+                }
+            };
+
+            this.removeItem = function (item) {
+                this.currentList.items.splice(this.currentList.items.indexOf(item), 1);
+            };
+
+            this.clearCompleted = function (selected) {
+                for (var key in selected) {
+                    if (this.currentList.items.indexOf(key) != -1 && selected[key]) {
+                        this.currentList.items.splice(this.currentList.items.indexOf(key), 1);
+                        delete selected[key];
+                    }
+                }
+
+                if (Object.keys(selected).length === 0) {
+                    selected = null;
+                }
+                return selected;
+            };
+
+            this.setSelected = function(selected)
+            {
+                this.selected = selected;
+            }
         });
 
-    function basePageController() {
-        this.testing = "Boo! From Controller!";
-        this.listArray = [];
-        this.currentList;
+
+    function basePageController(BasePageService) {
+        this.listArray = BasePageService.listArray;
+        this.currentList = BasePageService.currentList;
+        this.selected = BasePageService.selected;
+        this.testing = "Good Morning, Dave";
+        this.date = new Date();
         this.newList;
         this.newItem;
 
         this.addList = function () {
-            if (this.newList) {
-                var inList = false;
-                for (var i = 0; i < this.listArray.length; i++) {
-                    if (this.newList === this.listArray[i].name) {
-                        inList = true;
-                        break;
-                    }
-                }
-                if (!inList) {
-                    this.listArray.push({name: this.newList, items: []});
-                    this.currentList = this.listArray[this.listArray.length - 1];
-                }
-                this.newList = "";
-            }
+            BasePageService.addList(this.newList);
+            this.currentList = BasePageService.currentList;
+            this.newList = "";
         };
 
         this.getList = function (listName) {
-            for (var i = 0; i < this.listArray.length; i++) {
-                if (listName === this.listArray[i].name) {
-                    this.currentList = this.listArray[i];
-                    break;
-                }
-            }
+            BasePageService.getList(listName);
+            this.currentList = BasePageService.currentList;
         };
 
         this.clear = function () {
-            this.currentList.items = [];
+            BasePageService.clear();
             this.selected = null;
         };
 
         this.addItem = function () {
-            if (this.newItem && (this.currentList.items.indexOf(this.newItem) === -1)) {
-                this.currentList.items.push(this.newItem);
-                this.newItem = "";
-            }
+            BasePageService.addItem(this.newItem);
+            this.currentList = BasePageService.currentList;
+            this.newItem = "";
         };
 
         this.removeItem = function (item) {
-            this.currentList.items.splice(this.currentList.items.indexOf(item), 1);
+            BasePageService.removeItem(item);
         };
 
         this.clearCompleted = function () {
-            for (var key in this.selected) {
-                if (this.currentList.items.indexOf(key) != -1 && this.selected[key]) {
-                    this.currentList.items.splice(this.currentList.items.indexOf(key), 1);
-                    delete this.selected[key];
-                }
-            }
-            // if (Object.keys(this.selected).length === 0)
-            // {
-            //     this.selected = null;
-            // }
+            this.selected = BasePageService.clearCompleted(this.selected);
         };
+
+        this.sendSelected = function () {
+            BasePageService.setSelected(this.selected);
+        }
     }
 })();
